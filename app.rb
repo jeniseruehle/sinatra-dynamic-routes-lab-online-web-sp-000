cc4f356b9be364e2cb95c0d8c8c91dce14eeec66
Jenise Ruehle <jela07@gmail.com>
2020-10-09 20:28:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,9 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do 
     @number = params[:number]
     @phrase = params[:phrase]
-    number = @number.to_i 
+    number = @number.to_i
+    phrase = []
+    
     
   end 
 
