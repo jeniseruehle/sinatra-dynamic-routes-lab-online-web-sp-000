5ba808c3072c4bba4f4053aedbdc3d6c30d4db7d
Jenise Ruehle <jela07@gmail.com>
2020-10-09 20:12:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get "" do 
+    
+  end 
 
+  get "" do 
+    
+  end 
+  
+  get "" do 
+    
+  end 
+
+  get "" do 
+    
+  end 
+
+  get "" do 
+    
+  end 
 end
