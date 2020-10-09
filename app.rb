11a5f3c7b4c4b557497760025368b33a65247803
Jenise Ruehle <jela07@gmail.com>
2020-10-09 20:13:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,11 +13,11 @@ class App < Sinatra::Base
     
   end 
 
-  get "get '/say/:word1/:word2/:word3/:word4/:word5" do 
+  get "/say/:word1/:word2/:word3/:word4/:word5" do 
     
   end 
 
-  get "" do 
+  get "/:operation/:number1/:number2" do 
     
   end 
 end
